3b780d5b194c670a7b7267b7761e6bf4a5c153f6
Emre KARAGOZ <54494524+emrekrgz00@users.noreply.github.com>
2025-03-20 15:29:41 +0300

    Add files via upload

---
+++ Aykırı-Gözlem-20-03-25.py
@@ -0,0 +1,10 @@
+"""
+Veride genel eğilimin oldukça disina cikan ya da diğer gözlemlerden farkli olan gözlemlere AYKIRI GÖZLEM denir.
+
+Aykiriliği ifade eden nümerik değere, AYKIRI DEĞER denir.
+
+Aykiri değeri barindiran gözlem birimine AYKIRI GÖZLEM denir
+
+"""
+
+
---
+++ VeriÖnİşleme-1-20-03-25.py
@@ -0,0 +1,27 @@
+# Ayrıkı Gözlem  Analizi
+# Eksik veri Analizi
+# Standartlaştırma
+# Değişken Dönüşümleri
+
+"""
+Veri mi? Model mi?
+
+Makinen öğrenmesinin amaci belirli yapiyi ortaya koymaktir.
+"""
+
+"""
+Veri Temizleme
+    - Gürültürü Veri (Noisy Data)
+    - Eksik Veri Analizi (Missing Data Analysis)
+    - Aykiri Gözlem Analizi (Outlier Analysis)
+Veri Standardizasyonu 
+    - 0-1 Dönüşümü,
+    - z-skoruna dönüştürme,
+    - Logaritmik dönüşüm,
+Veri İndirgeme
+    - Gözlem sayisinin azaltilmasi
+    - Değişken sayisinin azaltilmasi
+Değişken Dönüşümleri
+    - Sürekli Değişken Dönüşümleri
+    - Kategorik Değişkenlerde Dönüşümler
+"""
